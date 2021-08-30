3358d8513aee2a72f38a34eb37e706460da29a77
dmitrymalykhin <54639719+dmitrymalykhin@users.noreply.github.com>
2021-08-30 22:31:08 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,44 @@
+//burger
+const btnBurger = document.getElementById('hamburger');
+
+btnBurger.addEventListener('click', function() {
+	if (document.body.classList.contains("hamburger-active")) {
+		closeBurger ();
+	} else {
+		openBurger ();
+	}
+})
+
+function closeBurger () {
+	document.body.classList.remove('hamburger-active');
+}
+
+function openBurger () {
+	document.body.classList.add('hamburger-active');
+}
+//End burger
+
+$('.m-menu-list-item a').on('click', function(){ 
+	if (document.body.classList.contains("hamburger-active")) {
+		closeBurger ();
+	} else {
+		openBurger ();
+	}
+});
+
+$('.open-modal').click(function(){
+	let modalEl = $(this).data('modal');
+	$('div[data-modal="'+modalEl+'"]').addClass('active');
+	$('div[data-modal="'+modalEl+'"]').find('.modal').addClass('active');
+});
+
+$('.close-modal').click(function(){
+	$(this).parents('.modal').removeClass('active');
+	$(this).parents('.modal-overlay').removeClass('active');
+});
+$(document).click(function (e) {
+	if ($(e.target).is('.modal-overlay')) {
+		$(e.target).removeClass('active');
+		$(e.target).find('.modal').removeClass('active');
+	}
+});
